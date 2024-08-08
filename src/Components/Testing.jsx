8794f85e2f93b395df574ebb756bdedf51716aa2
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2024-08-08 16:11:35 -0400

    First Git, CSS setup

---
+++ src/Components/Testing.jsx
@@ -0,0 +1,12 @@
+export default function Homepage() {
+  return (
+    <section className="h-lvh overflow-x-hidden">
+      <div className="fixed bg-[#131313] h-lvh w-full object-cover -z-10" />
+      <img
+        src="/mainBG.png"
+        alt="cover-bg"
+        className="fixed h-lvh w-full object-cover -z-10 object-center"
+      />
+    </section>
+  );
+}
