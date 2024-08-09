0af129e161e2f2240909185ee7133cabd73a5104
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2024-08-08 21:36:43 -0400

    Initial Template

---
+++ src/Components/CustomWrap/AccordCustom.jsx
@@ -0,0 +1,63 @@
+import { ThemeProvider } from "@material-tailwind/react";
+
+export default function AccordWrap({ children }) {
+  const customTheme = {
+    accordion: {
+      defaultProps: {
+        icon: undefined,
+        className: "",
+        animate: {
+          unmount: {},
+          mount: {},
+        },
+        disabled: false,
+      },
+      styles: {
+        base: {
+          container: {
+            display: "block",
+            position: "relative",
+            width: "w-full",
+          },
+          header: {
+            initial: {
+              display: "flex",
+              justifyContent: "justify-between",
+              alignItems: "items-center",
+              width: "w-full",
+              py: "py-2",
+              borderWidth: "border-none",
+              color: "text-white",
+              fontSmoothing: "antialiased",
+              fontFamily: "font-sans",
+              fontSize: "text-xl",
+              textAlign: "text-left",
+              fontWeight: "font-semibold",
+              lineHeight: "leading-snug",
+              userSelect: "select-none",
+              hover: "",
+              transition: "transition-colors",
+            },
+            active: { color: "text-white" },
+            icon: {
+              ml: "ml-4",
+            },
+          },
+          body: {
+            display: "block",
+            width: "w-full",
+            py: "py-2",
+            color: "text-white",
+            fontSmoothing: "antialiased",
+            fontFamily: "font-sans",
+            fontSize: "text-sm",
+            fontWeight: "font-light",
+            lineHeight: "leading-normal",
+          },
+        },
+      },
+    },
+  };
+
+  return <ThemeProvider value={customTheme}>{children}</ThemeProvider>;
+}
--- src/Components/PitLadder.jsx
+++ src/Components/PitLadder.jsx
@@ -5,13 +5,30 @@ import {
   List,
   ListItem,
   Avatar,
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
 } from "@material-tailwind/react";
 
 import { useData } from "../Hook/DataFetch";
 import DataLoadingLoader from "../Hook/Loader";
+import { useState } from "react";
+import AccordWrap from "./CustomWrap/AccordCustom";
+
+const testingSkills = [
+  `fire_bolt`,
+  `fireball`,
+  `firewall`,
+  `flame_shield`,
+  `frost_bolt`,
+  `teleport`,
+];
 
 export default function PitLadder() {
   const { posts, loader } = useData();
+  const [open, setOpen] = useState(0);
+
+  const handleOpen = (value) => setOpen(open === value ? null : value);
 
   const displayData = posts;
 
@@ -20,64 +37,104 @@ export default function PitLadder() {
       {loader ? (
         <DataLoadingLoader />
       ) : (
-        <Card className="w-full mx-auto max-w-[1400px] my-5 border-[1px] border-[#131111] bg-transparent backdrop-blur-md">
-          <div className="text-[48px] text-center my-5 text-[white] font-customDiablo">
-            Diablo 4: The Pit
-          </div>
-          <List>
-            <ListItem className="select-none pointer-events-none flex justify-between">
-              <div className="text-[white] text-[18px] flex-1 text-center font-customDiablo ">
+        <AccordWrap>
+          <Card className="w-full mx-auto max-w-[1000px] my-10 px-2 bg-transparent">
+            <div className="text-[48px] text-center my-5 text-[white] font-customDiablo">
+              Diablo 4: The Pit
+            </div>
+
+            <div className="p-2 mb-1 flex">
+              <div className="text-[white] text-[16px] flex-1 text-center font-customDiablo ">
                 Player
               </div>
-              <div className="text-[white] text-[18px] flex-1 text-center font-customDiablo ">
+              <div className="text-[white] text-[16px] flex-1 text-center font-customDiablo ">
                 Class
               </div>
-              <div className="text-[white] text-[18px] flex-1 text-center font-customDiablo ">
+              <div className="text-[white] text-[16px] flex-1 text-center font-customDiablo ">
                 Tier
               </div>
-              <div className="text-[white] text-[18px] flex-1 text-center font-customDiablo ">
-                Build
-              </div>
-              <div className="text-[white] text-[18px] flex-1 text-center font-customDiablo ">
+              <div className="text-[white] text-[16px] flex-1 text-center font-customDiablo ">
                 Time
               </div>
-              <div className="text-[white] text-[18px] flex-1 text-center font-customDiablo ">
-                Video
-              </div>
-            </ListItem>
-            {displayData.map((obj, index) => (
-              <ListItem className="select-none pointer-events-none flex justify-between bg-transparent">
-                <div className="text-white flex-1 text-center">
-                  {obj.Player}
-                </div>
-                <div className="flex-1 text-center">
-                  <Avatar src={`/Classes/${obj.Class}.png`} />
-                </div>
-                <div className="text-white flex-1 text-center">{obj.Tier}</div>
-                <div className="text-white flex-1 text-center">{obj.Build}</div>
-                <div className="text-white flex-1 text-center">{obj.Time}</div>
+            </div>
 
-                <div className="flex-1 flex justify-center">
-                  <a href={obj.Video} target="_blank">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                      color="white"
-                      className="w-6 h-6 z-10"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  </a>
-                </div>
-              </ListItem>
+            {displayData.map((obj, index) => (
+              <div
+                className={`${
+                  index % 2 === 0 ? `bg-[#222222cc]` : `bg-[#141414cc]`
+                } mb-1 rounded-lg backdrop-blur-sm relative`}
+              >
+                <Accordion open={open === index}>
+                  <AccordionHeader
+                    onClick={() => handleOpen(index)}
+                    className="flex justify-between"
+                  >
+                    <div className="absolute top-2 left-2 text-[12px] font-[Roberto]">
+                      {index + 1}
+                    </div>
+                    <div className="flex-1 text-center font-customDiablo text-[16px]">
+                      {obj.Player}
+                    </div>
+                    <div className="flex-1 flex justify-center">
+                      <Avatar src={`/Classes/${obj.Class}.png`} />
+                    </div>
+                    <div className="flex-1 text-center font-customDiablo text-[18px]">
+                      {obj.Time}
+                    </div>
+                    <div className="flex-1 text-center font-customDiablo text-[18px]">
+                      {obj.Tier}
+                    </div>
+                  </AccordionHeader>
+
+                  <AccordionBody className="flex justify-evenly">
+                    <div>
+                      <div className="text-center font-customDiablo text-[16px]">
+                        Build: {obj.Build}
+                      </div>
+                      <div className="flex-1 flex flex-wrap gap-1 justify-center w-full">
+                        {testingSkills.map((item) => (
+                          <Avatar
+                            src={`/Skills/${item}.jpg`}
+                            variant="rounded"
+                            size="sm"
+                          />
+                        ))}
+                      </div>
+                    </div>
+
+                    <div className="flex items-center gap-5 justify-between">
+                      <div className="flex flex-col items-center p-2">
+                        <a
+                          href="https://maxroll.gg/d4/planner/"
+                          target="_blank"
+                          className="flex-1 font-customDiablo text-[16px] text-red-500"
+                        >
+                          Video Link
+                        </a>
+                        <a
+                          href="https://maxroll.gg/d4/planner/"
+                          target="_blank"
+                          className="flex-1 font-customDiablo text-[16px] text-light-blue-500"
+                        >
+                          Build Link
+                        </a>
+                      </div>
+
+                      <div className="hidden sm:block p-2">
+                        <div className="flex-1 text-center font-customDiablo text-[18px]">
+                          {obj.Mode === "SC" ? "Softcore" : "Hardcore"}
+                        </div>
+                        <div className="flex-1 text-center font-customDiablo text-[18px]">
+                          8/6/2024
+                        </div>
+                      </div>
+                    </div>
+                  </AccordionBody>
+                </Accordion>
+              </div>
             ))}
-          </List>
-        </Card>
+          </Card>
+        </AccordWrap>
       )}
     </>
   );
