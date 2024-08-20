7b8027aad746aff0ab1fee3187ef3fabdfcf39b6
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2024-08-20 10:40:10 -0400

    Git Touch: BG

--- src/Button/ClassesBtn.jsx
+++ src/Button/ClassesBtn.jsx
@@ -1,5 +1,4 @@
 import { Button, Avatar } from "@material-tailwind/react";
-import { classColor } from "../Components/Accordion";
 
 export default function ClassesBtn({ onButtonClick, classes }) {
   const allClasses = [
@@ -32,16 +31,15 @@ export default function ClassesBtn({ onButtonClick, classes }) {
             variant={onButtonClick(index + 4).variant}
             onClick={onButtonClick(index + 4).onClick}
             color="red"
-            className="p-2 min-w-[120px] font-customDress text-[14px] shadow-[0_0_20px_red] relative border-[2px] border-[#28282b] rounded-lg bg-[#131111] group hover:scale-105 duration-200 ease-in transition-all"
-            // style={{ backgroundColor: classColor(num) }}
+            className="p-2 min-w-[120px] font-customDress text-[14px] shadow-[0_0_20px_red] relative border-[2px] border-[#28282b] rounded-lg bg-[transparent] group hover:scale-105 duration-200 ease-in transition-all"
           >
             {/* <div className="absolute bg-[black] h-full w-full top-0 left-0 -z-10" /> */}
-            {/* <img
-              src={`/bg/c${index + 1}.png`}
+            <img
+              src={`/bg/class.png`}
               alt="cover-bg"
               className="absolute h-full w-full top-0 left-0 object-cover -z-10 opacity-30"
-            /> */}
-            <div className="flex flex-col justify-center items-center group-hover:text-[white] ease-in duration-200 transition-colors">
+            />
+            <div className="flex flex-col justify-center items-center group-hover:text-[red] ease-in duration-200 transition-colors">
               <Avatar
                 src={`/Classes/${num}.png`}
                 size="sm"
