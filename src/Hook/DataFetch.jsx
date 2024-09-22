3aa194eb6215d24b3cea664215772b8017a6a3b4
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2024-09-22 11:31:40 -0400

    Revert

--- src/Hook/DataFetch.jsx
+++ src/Hook/DataFetch.jsx
@@ -46,7 +46,9 @@ const TestingFetch = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("/api/data"); // Use your Worker preview URL
+        const response = await fetch(
+          "https://my-worker.diablo4pit.workers.dev/api/data"
+        ); // Use your Worker preview URL
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://my-worker.diablo4pit.workers.dev/api/data', // Your Cloudflare Worker URL
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''), // Optional: rewrite the path if needed
-      },
-    },
-  },
 })
